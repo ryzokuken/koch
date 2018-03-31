82386b9c6845b1e5ad22ea7caaf9878efd918d05
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-31 19:40:56 +0530

    Add basic command handling

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", &args[1..]);
+    match args[1].as_str() {
+        "install" => println!("PASS!"),
+        _ => panic!("FAIL!"),
+    }
 }
