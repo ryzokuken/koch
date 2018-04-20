b9c3a735c638e6dd2dcf2fef78a99720ce5359f6
Ujjwal Sharma <usharma1998@gmail.com>
2018-04-20 19:00:49 +0530

    JSON works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+extern crate reqwest;
+extern crate serde_json;
+
 use std::env;
+use serde_json::Value;
+
+fn get_text(website: &str) {
+    let text = reqwest::get(website).unwrap().text().unwrap();
+    let v: Value = serde_json::from_str(&text).unwrap();
+    println!("hello {}", v["hello"].as_str().unwrap());
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
-        "install" => println!("PASS!"),
+        "install" => get_text("https://api.myjson.com/bins/1gq93v"),
         _ => panic!("FAIL!"),
     }
 }
